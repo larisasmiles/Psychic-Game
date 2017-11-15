1356dfd2988638361536488489f33298d1bab422
Larisasmiles <larisasmiles@gmail.com>
2017-11-14 18:24:46 -0700

    updated conditionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,38 @@
  
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
+  var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var wins = 0;
   var losses = 0;
   var guessesLeft = 10;
-  var guessCount = "";
-  var lettersPressed = [];
+  var guessCount = " ";
   var letter;
   
 
-function randomLetter() {
-  var pickedLetters = letters[Math.floor(Math.random() * letters.length)];
-  console.log(pickedLetters);
-  lettersPressed
+  function randomLetter() {
 
+  var lettersGuessed = [Math.floor(Math.random() * letters.length)];
+  letter = letters[lettersGuessed];
+  return letter;
 }
 
-document.onkeyup = function(event) {
+  document.onkeyup = function(event) {
+  var lettersGuessed = []
   var userGuess = event.key;
-  lettersPressed += userGuess + ',';
+  lettersGuessed += userGuess + ',';
 
-  if(guessesLeft > 0 && userGuess != letter) {
-    guessesLeft --;
+  if(lettersGuessed === letter) {
+    alert("You can read my mind!")
+    reset 
   }
-  else if (guessesLeft === 0 && userGuess != letter) {
+  else if (lettersGuessed != letter && guessesLeft > 1) {
+    alert("You can't read my mind! Try again!")
     losses ++;
-    guessesLeft = 10;
-    lettersPressed = " ";
+    guessesLeft --;
+    lettersGuessed << letter;
     randomLetter();
+  }
+  else {
+  
+    alert("Don't quit your day job! You lose!")
   }
 }
 
