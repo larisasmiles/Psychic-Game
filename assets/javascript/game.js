ef13f88a9517dcc01cb1373ea0fc07ab1c9d9fb4
Larisasmiles <larisasmiles@gmail.com>
2017-11-18 20:53:18 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,69 @@
- 
-  var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var wins = 0;
-  var losses = 0;
-  var guessesLeft = 10;
-  var guessCount = " ";
-  var letter;
-  
+console.log("working");
+//alphabet array
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  function randomLetter() {
+//variables for answer, wins, losses, guessesLeft and guesses
+var answer;
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guesses = [];
 
-  var lettersGuessed = [Math.floor(Math.random() * letters.length)];
-  letter = letters[lettersGuessed];
-  return letter;
-}
 
-  document.onkeyup = function(event) {
-  var lettersGuessed = []
+//computer chooses a letter
+var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(answer)
+
+//user guesses the letter
+document.onkeypress = function (event) {
   var userGuess = event.key;
-  lettersGuessed += userGuess + ',';
+  guesses.push(userGuess);
 
-  if(lettersGuessed === letter) {
-    alert("You can read my mind!")
-    reset 
-  }
-  else if (lettersGuessed != letter && guessesLeft > 1) {
-    alert("You can't read my mind! Try again!")
-    losses ++;
-    guessesLeft --;
-    lettersGuessed << letter;
-    randomLetter();
+  if (userGuess === answer) {
+    win();
   }
   else {
-  
-    alert("Don't quit your day job! You lose!")
+    guessesLeft--;
   }
-}
+
+  if (guessesLeft == 0) {
+    loss();
+  }
+
+  //function if user wins
+  function win() {
+    wins++;
+    reset();
+  }
+
+  //function if user losses
+  function loss() {
+    losses++;
+    reset();
+  }
+
+  //game reset
+  function reset() {
+    answer = alphabet[Math.floor(Math.random() * alphabet.length)]
+    console.log(answer);
+    guessesLeft = 9;
+    guesses = [];
+  }
+};
+
+var html =
+"<p>Wins: " + wins + "</p>" +
+"<p>Losses: " + losses + "</p>" +
+"<p>Guesses Left: " + guessesLeft + "</p>" +
+"<p>Your Guesses SO far: " + guesses + "</p>";
 
 
+// Set the inner HTML contents of the #game div to our html string
+// document.querySelector("#game").innerHTML = html;
+// I have been trying to get this to log to my HTML. 
+// //displays in html
+document.queryselector("#wins").innerHTML = html;
+  document.getElementById('losses').innerHTML = "Losses: " + losses;
+  document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+  document.getElementById('guesses').innerHTML = "Your guesses so far: " + guesses;
 
