965e8676c24d7e526141873de6f83a0685b50a3e
Larisasmiles <larisasmiles@gmail.com>
2018-03-22 07:52:23 -0600

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,62 @@
-console.log("working");
-//alphabet array
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-//variables for answer, wins, losses, guessesLeft and guesses
-var answer;
+// define variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var guesses = [];
+var guesses = 9;
+var allGuesses = [];
+
+var randomLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var computerGuess = randomLetter[Math.floor(Math.random() * 25)];
+
+//display the appropriate value of wins, losses, and guesses
+
+document.getElementById("wins-span").innerHTML = " " + wins;
+document.getElementById("losses-span").innerHTML = " " + losses;
+document.getElementById("guesses-span").innerHTML = " " + guesses;
+
+//reset function
+function reset() {
+  guesses = 9;
+  document.getElementById("guesses-span").innerHTML = guesses;
+  allGuesses = [];
+  document.getElementById("guessesMade").innerHTML = allGuesses;
+  computerGuess = randomLetter[Math.floor(Math.random() * 25)];
+}
 
 
-//computer chooses a letter
-var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(answer)
+// when a key is released, populate the variable and write it to the screen
 
-//user guesses the letter
-document.onkeypress = function (event) {
+document.onkeyup = function(event) {
+
   var userGuess = event.key;
-  guesses.push(userGuess);
 
-  if (userGuess === answer) {
-    win();
-  }
-  else {
-    guessesLeft--;
-  }
+  console.log("User: " + userGuess);
+  console.log("Computer: " + computerGuess);
 
-  if (guessesLeft == 0) {
-    loss();
-  }
+//pushed the userGuess from the keyboard into the array, and then displays these choices on the screen.
+  allGuesses.push(userGuess);
+  document.getElementById("guessesMade").innerHTML = allGuesses;
 
-  //function if user wins
-  function win() {
+//change the wins and losses counters based on the selections
+  if (userGuess === computerGuess) {
     wins++;
-    reset();
-  }
+    document.getElementById("wins-span").innerHTML = wins;
+    guesses = 9;
+    document.getElementById("guesses-span").innerHTML = guesses;
+    allGuesses = [];
+    document.getElementById("guessesMade").innerHTML = allGuesses;
+    document.getElementById("correct").innerHTML = " " + computerGuess;
+  } else {
+    guesses--;
+    document.getElementById("guesses-span").innerHTML = guesses;
+  };
 
-  //function if user losses
-  function loss() {
+  if (guesses === 0) {
     losses++;
+    document.getElementById("losses-span").innerHTML = losses;
+    document.getElementById("correct").innerHTML = " " + computerGuess;
     reset();
   }
 
-  //game reset
-  function reset() {
-    answer = alphabet[Math.floor(Math.random() * alphabet.length)]
-    console.log(answer);
-    guessesLeft = 9;
-    guesses = [];
-  }
+  
 };
-
-var html =
-"<p>Wins: " + wins + "</p>" +
-"<p>Losses: " + losses + "</p>" +
-"<p>Guesses Left: " + guessesLeft + "</p>" +
-"<p>Your Guesses SO far: " + guesses + "</p>";
-
-
-// Set the inner HTML contents of the #game div to our html string
-// document.querySelector("#game").innerHTML = html;
-// I have been trying to get this to log to my HTML. 
-// //displays in html
-document.queryselector("#wins").innerHTML = html;
-  document.getElementById('losses').innerHTML = "Losses: " + losses;
-  document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
-  document.getElementById('guesses').innerHTML = "Your guesses so far: " + guesses;
-
